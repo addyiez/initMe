1f9082ce74c039da64017f290143e8b7da23f629
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-11 23:34:01 +0530

    App.java added

---
+++ javaBasics.java
@@ -0,0 +1,8 @@
+public class javaBasics {
+    public static void main(String args[])
+    {
+       System.out.print("hello world");
+    }
+    
+}
+//boilerplate code
---
+++ project1.java
@@ -0,0 +1,6 @@
+public class project1 {
+    
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
