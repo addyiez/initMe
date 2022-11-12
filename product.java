2ce140f43eb9da4ea6a1370c3615745fe4a0ac29
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-12 18:21:44 +0530

    add product.java

--- product.java
+++ product.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class product {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int pro =a*b;
+        System.out.println(pro);
+        sc.close();
+    }
     
 }
