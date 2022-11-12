3f3cbb4ccf849311c37e4b1f3264cb59131ce9d5
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-12 18:35:25 +0530

    areaofcircle

---
+++ AOC.java
@@ -0,0 +1,17 @@
+//import java.io.InputStream;
+import java.util.Scanner;
+
+public class AOC {
+    public static void main(String args[]){
+
+        Scanner sc=new Scanner(System.in);
+        float r=sc.nextFloat();
+        double area=3.14*r*r;
+        System.out.println(area);
+        sc.close();
+    }
+
+   /*  private static Scanner newScanner(InputStream in) {
+        return null;
+    }*/
+}
