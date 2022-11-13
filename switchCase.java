f80870ac181b8fcc2ac1cb2d25829199e27e6412
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-14 01:35:11 +0530

    .

---
+++ switchCase.java
@@ -0,0 +1,17 @@
+public class switchCase {
+
+    public static void main(String args[]){
+
+        int number=3;
+        switch(number){
+            case 1: System.out.println("samosa");
+            break;
+            case 2: System.out.println("pizza");
+            break;
+            case 3: System.out.println("burger");
+            break;
+            default : System.out.println("nothing will serve");
+        }
+    }
+    
+}
