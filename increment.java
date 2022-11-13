7eab5b70c02efdce0adfb45a139defa01b02719b
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-13 22:45:06 +0530

    added something

--- increment.java
+++ increment.java
@@ -1,4 +1,5 @@
 public class increment {
+    
     public static void main(String args []){
         int a=10;
         int b=++a; //pre increment
@@ -7,8 +8,11 @@ public class increment {
 
         int c=10;
         int d=c++; //post increment
+        int e=5;
+         e*=5;// or e=e*e; or e=e*5;
         System.out.println("\n"+c);
         System.out.println(d);
+        System.out.println(e);
 
     }
     
