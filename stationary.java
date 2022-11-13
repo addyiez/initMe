f504336970c830c892dd097064c00bd92f952589
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-14 01:00:58 +0530

    changed

--- stationary.java
+++ stationary.java
@@ -3,8 +3,11 @@ public class stationary {
     public static void main(String args[])
     {
          Scanner sc=new Scanner(System.in);
+         System.out.println("price of pencil: ");
          float pencil=sc.nextFloat();
+         System.out.println("price of pen: ");
          float pen=sc.nextFloat();
+         System.out.println("price of eraser: ");
          float eraser=sc.nextFloat();
          float bill= pencil+pen+eraser;
          float tax=bill*0.18f; //include 18%gst
