8840508a868a71207ac9dbcb58810747051f8d89
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-14 01:44:30 +0530

    new

---
+++ calculator.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class calculator {
+    
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter a:");
+        int a= sc.nextInt();
+        System.out.println("enter b:");
+        int b= sc.nextInt();
+        System.out.println("enter operator:");
+        char operator = sc.next().charAt(0);
+
+        switch (operator){
+
+            case '+' : System.out.println(a+b);
+            break;
+            case '-' : System.out.println(a-b);
+            break;
+            case '*' : System.out.println(a*b);
+            break;
+            case '/' : System.out.println(a/b);
+            break;
+            default : System.out.println("invalid choice");
+            sc.close();
+
+        }
+    }
+}
