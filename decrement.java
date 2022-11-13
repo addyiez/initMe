8d2827179d7ce2edbe8d25301c627b63c33438eb
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-13 22:25:25 +0530

    today's beginning

---
+++ decrement.java
@@ -0,0 +1,15 @@
+public class decrement {
+    public static void main(String args[]){
+        int a=10;
+        int b=--a;
+        System.out.println(a);
+        System.out.println(b);
+
+        int c=10;
+        int d=c--;
+        System.out.println("\n"+c);
+        System.out.println(d);
+    }
+
+    
+}
---
+++ increment.java
@@ -0,0 +1,15 @@
+public class increment {
+    public static void main(String args []){
+        int a=10;
+        int b=++a; //pre increment
+        System.out.println(a);
+        System.out.println(b);
+
+        int c=10;
+        int d=c++; //post increment
+        System.out.println("\n"+c);
+        System.out.println(d);
+
+    }
+    
+}
