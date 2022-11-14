b8ce24846fb158cc4f1575bfc73302adc893be5c
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-15 04:09:45 +0530

    end

---
+++ loop/SquarePattern.java
@@ -0,0 +1,10 @@
+package loop;
+public class SquarePattern {
+    public static void main(String args[]){
+
+        for(int line=1; line<=4; line++){
+        System.out.println("* * * *");
+        }
+    }
+    
+}
---
+++ loop/reverse.java
@@ -0,0 +1,38 @@
+package loop;
+import java.util.*;
+/*
+public class reverse {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter any number:");
+        int n = sc.nextInt();
+        System.out.print("reverse: ");
+        while(n>0){
+            int lastDigit=n%10;
+            System.out.print(lastDigit +"");
+            n/=10; //n=n/10;
+        }
+        System.out.println();
+        sc.close();
+        
+    }
+    
+}
+*/
+public class reverse {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter any number:");
+        int n = sc.nextInt();
+        int rev=0;
+        while(n>0){
+            int lastDigit=n%10;
+            rev=rev*10+lastDigit;
+            n/=10;
+        }
+        System.out.print("reverse: "+rev);
+        sc.close();
+        
+    }
+    
+}
