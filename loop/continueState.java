a1b6689a3b66ad43cf156853a289e8e550a3d878
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-16 02:00:05 +0530

    second

---
+++ loop/continueState.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class continueState {
+    public static void main(String args[]){
+       for(int i=1; i<=5; i++){
+        if(i==3){
+            continue;
+        }
+        System.out.println(i);
+       }
+    }
+    
+}
