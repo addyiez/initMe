fba79eaf24f507c322eace766014e9e87a5479aa
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-16 01:56:26 +0530

    starting

---
+++ loop/breakstate.java
@@ -0,0 +1,20 @@
+package loop;
+import java.util.*;
+
+public class breakstate {
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+
+        do{
+            System.out.print("enter number: ");
+            int n = sc.nextInt();
+            if (n%10==0){
+                break;
+            }
+            System.out.println(n);
+        }
+        while(true);
+        sc.close();
+    }
+    
+}
