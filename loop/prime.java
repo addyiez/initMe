d70623227ab74948947e7f89a7282f6c5a58fd88
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-16 02:55:51 +0530

    prime

---
+++ loop/prime.java
@@ -0,0 +1,36 @@
+package loop;
+
+import java.util.*;
+public class prime {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter a number: ");
+        int n=sc.nextInt();
+        if(n==2){
+            System.out.println("number is prime");
+        }
+        else{
+            boolean isPrime=true;
+            for(int i=2; i<=Math.sqrt(n); i++){
+                if(n%i==0){
+                    isPrime = false;
+                }
+            }
+            if(isPrime==true){
+                System.out.println("n is prime");
+            }
+            else {
+                System.out.println("number is not prime");
+            }
+        }
+        sc.close();
+     /*  
+     Trying something
+      for(int i=2 ; i<=n ; i++){
+            if(n%i==0){
+                System.out.println("number is not prime: "+n);
+
+            }
+        }*/
+    }
+}
