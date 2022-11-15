40bd1c5e27159c305c35fea75043fe5fbb46bd11
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-16 02:14:13 +0530

    modify

--- loop/continueState.java
+++ loop/continueState.java
@@ -1,13 +1,32 @@
 package loop;
-
 public class continueState {
     public static void main(String args[]){
+
        for(int i=1; i<=5; i++){
         if(i==3){
             continue;
         }
         System.out.println(i);
        }
+       
+    }
+    
+}/*
+import java.util.*;
+public class continueState {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+     do{
+        System.out.println("enter numbers: ");
+        int n=sc.nextInt();
+        if(n%10==0){
+            continue;
+        }
+        System.out.print("number: "+n);  
+     }while(true);
+     
     }
     
 }
+*/
