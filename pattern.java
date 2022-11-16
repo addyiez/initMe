37d6f20348bc87a4e29f14cdfecd941d186ea069
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-17 02:19:07 +0530

    pattern added

---
+++ pattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class pattern {
+    
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter number of lines: ");
+        int n=sc.nextInt();
+        for(int line=1; line<=n; line++){
+            for(int star=1;star<=line;star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
