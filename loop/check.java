02bf48624f7ad396dadc6d68ea09529a969d11dc
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-17 00:09:37 +0530

    .

---
+++ loop/check.java
@@ -0,0 +1,22 @@
+package loop;
+/*
+public class check {
+
+        public static void main(String[] args){
+        for(int i=0; i<5; i++) {
+        System.out.println("Hello");
+        i+=2;
+        }
+        }
+
+}*/
+public class check {
+    public static void main(String args[]) {
+        int i;
+    for(i = 0; i <= 5; i++ ) {
+    System.out.println("i = " + i );
+    }
+    System.out.println("i after the loop = " + i );
+    }
+    }
+   
