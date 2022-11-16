8c48dc5a5f9472e722f239e43a54c96a344d409e
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-17 01:48:21 +0530

    factorial

--- loop/factorial.java
+++ loop/factorial.java
@@ -1,4 +1,17 @@
 package loop;
+import java.util.*;
 public class factorial {
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        System.out.print("enter a positive number : ");
+        int n=sc.nextInt();
+        int fact=1;
+        for(int i=1; i<=n; i++){
+            fact*=i; //fact=fact*i;
+
+        } 
+        System.out.println("factorial of "+n+" is: "+fact);
+        sc.close();
+    }
     
 }
