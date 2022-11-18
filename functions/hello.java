e8c9d8270b93510515980eb295716f5c48cd0adc
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-18 23:35:28 +0530

    function

---
+++ functions/hello.java
@@ -0,0 +1,9 @@
+package functions;
+public class hello{
+    public static void printHello(){
+        System.out.println("hello world");
+    }
+    public static void main(String args[]){
+        printHello();
+    }
+}
