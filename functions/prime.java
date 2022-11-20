e90d63bcfbe4bf13350882dafe0f7a885f8ad9b0
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-21 04:47:48 +0530

    modified file

--- functions/prime.java
+++ functions/prime.java
@@ -1,9 +1,9 @@
 package functions;
-
 public class prime {
 
-    public static boolean isPrime(int n){
-     /*   boolean isPrime=true;
+     /*
+     public static boolean isPrime(int n){   
+     boolean isPrime=true;
         for(int i=2; i<=n-1;i++){
             if(n%2==0){
                 isPrime=false;
@@ -15,7 +15,23 @@ public class prime {
         //OR
 
         */
-          for(int i=2; i<=n-1;i++){
+        //corner cases
+        
+        public static boolean isPrime(int n){
+
+            //1
+
+            if (n==2){
+                return true;
+            }
+
+            //2
+
+            for(int i=2; i<=n-1; i++){ 
+            
+                //optimized
+
+            //for(int i=2; i<=Math.sqrt(n); i++){
             if(n%2==0){
                 return false;
             }
