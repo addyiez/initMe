e44556c9d364ebaf0f43b10400bb97dddc55a817
addyiez <88774507+addyiez@users.noreply.github.com>
2022-11-21 23:10:18 +0530

    file add

--- functions/palindrome.java
+++ functions/palindrome.java
@@ -1,5 +1,30 @@
 package functions;
-
+import java.util.Scanner;
 public class palindrome {
-    
+    public static boolean isPalindrome(int number){
+        int palindrome=number;
+        int reverse=0;
+        while(palindrome!=0){
+           int remainder=palindrome%10;
+           reverse=reverse*10+remainder;
+           palindrome=palindrome/10;
+        }
+        if (number==reverse){
+            return true;
+        }
+        return false;
+    }
+ 
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n=sc.nextInt();
+        if(isPalindrome(n)){
+            System.out.println("number : "+n+ " is a palindrome");
+        }
+        else{
+            System.out.println("number : "+n+ " is not a palindrome");
+        }
+        sc.close();
+    }
 }
