fde16d737377a70d1ff88b4b68330503963747e1
addyiez <88774507+addyiez@users.noreply.github.com>
2022-12-29 00:01:25 +0530

    add

---
+++ pattern/hollowRhombus.java
@@ -0,0 +1,26 @@
+package pattern;
+
+public class hollowRhombus {
+    public static void rhomH(int n){
+        for(int i=1; i<=n; i++){
+            //spaces
+            for(int j=1; j<=(n-i); j++){
+                System.out.print(" ");
+            }
+            //hollow rectangle - stars
+            for(int j=1; j<=n; j++){
+                if(i==1 || i==n || j==1 || j==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        rhomH(5);
+    }
+    
+}
