269aceef6959e5614dd58828fd5362592a2f7d07
addyiez <88774507+addyiez@users.noreply.github.com>
2022-12-28 23:27:37 +0530

    file added

---
+++ pattern/butterfly.java
@@ -0,0 +1,45 @@
+package pattern;
+
+public class butterfly {
+    public static void butterflyP(int n){
+        //1st half
+        for(int i=1; i<=n; i++){
+
+            //stars - i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            //spaces 2*(n-1)
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+
+            //stars - i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }  
+            System.out.println();
+        }
+        //2nd half
+        for(int i=n; i>=1; i--){
+            //stars - i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            //spaces 2*(n-1)
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+
+            //stars - i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }  
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        butterflyP(4);
+    }
+    
+}
