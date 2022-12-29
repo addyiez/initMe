6e1fb39584a9101f097d4d84c8e6e1fe3a2d80ad
addyiez <88774507+addyiez@users.noreply.github.com>
2022-12-29 17:16:18 +0530

    modify

--- array/creation.java
+++ array/creation.java
@@ -5,6 +5,10 @@ public class creation {
     public static void main(String args[]){
         int marks[]=new int[50];
         Scanner sc =new Scanner(System.in);
+
+        //array length check
+        System.out.println("length of array = "+marks.length);
+        
         marks[0]=sc.nextInt(); //phy
         marks[1]=sc.nextInt(); //chem
         marks[2]=sc.nextInt(); //math
@@ -12,6 +16,8 @@ public class creation {
         System.out.println("phy : " + marks[0]);
         System.out.println("chem : " + marks[1]);
         System.out.println("math : " + marks[2]);
+        int percentage = ((marks[0]+marks[1]+marks[2])/3);
+        System.out.println("percentage : " + percentage + "%");
         sc.close();
     }
     
