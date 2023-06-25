01e2c730a86706ae7061dc45b7c2d0a6c8276a50
addyiez <88774507+addyiez@users.noreply.github.com>
2023-06-25 15:21:11 +0530

    correction

--- functions/palindrome.java
+++ functions/palindrome.java
@@ -4,7 +4,7 @@ public class palindrome {
     public static boolean isPalindrome(int number){
         int palindrome=number;
         int reverse=0;
-        while(palindrome!=0){
+        while(palindrome>-0){
            int remainder=palindrome%10;
            reverse=reverse*10+remainder;
            palindrome=palindrome/10;
