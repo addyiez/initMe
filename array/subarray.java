3b689f998d24b7501c926021b6e503adc05475e1
addyiez <88774507+addyiez@users.noreply.github.com>
2023-06-25 16:30:39 +0530

    copy

---
+++ array/subarray.java
@@ -0,0 +1,17 @@
+package array;
+
+public class subarray {
+    public static void printSub(int numbers[]) {
+        for(int i=0; i<numbers.length; i++){
+            for(int j=i; j<numbers.length; j++){
+                for(int k=i;k<=j;k++){
+                    System.out.print(numbers[k]+" ");
+                }System.out.println();
+            }System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        int numbers[]={2, 4, 6, 8, 10};
+        printSub(numbers);
+    }
+}
