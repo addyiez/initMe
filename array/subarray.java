b03db9679b05e5fc4b3e98598cb235576d78a5be
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-04 14:27:39 +0530

    here we go again

--- array/subarray.java
+++ array/subarray.java
@@ -2,13 +2,16 @@ package array;
 
 public class subarray {
     public static void printSub(int numbers[]) {
+        int ts=0;
         for(int i=0; i<numbers.length; i++){
             for(int j=i; j<numbers.length; j++){
                 for(int k=i;k<=j;k++){
                     System.out.print(numbers[k]+" ");
-                }System.out.println();
+                }
+                ts++;
+                System.out.println();
             }System.out.println();
-        }
+        }System.out.println("total subarrays = " + ts);
     }
     public static void main(String args[]){
         int numbers[]={2, 4, 6, 8, 10};
