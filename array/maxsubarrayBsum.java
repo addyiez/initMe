d25e50526336d650e307d072c4f3c431ce20df92
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-06 16:44:52 +0530

    brute force

---
+++ array/maxsubarrayBsum.java
@@ -0,0 +1,31 @@
+package array;
+
+public class maxsubarrayBsum { //BruteForce TIME COMPLEXITY =O(n^3)
+    public static void printSum(int numbers[]){
+         int currSum=0;
+         int maxSum=Integer.MIN_VALUE;
+         for(int i=0;i<numbers.length;i++){
+            int start = i;
+            for(int j=i;j<numbers.length;j++){
+                int end =j;
+                currSum=0;
+                for(int k=start; k<=end; k++){
+                    currSum += numbers[k];
+                } 
+                System.out.println(currSum);               
+                if(maxSum<currSum){
+                    maxSum=currSum;
+                }
+
+            }
+         } 
+         System.out.println("max sum = " +maxSum);
+
+    }
+
+    public static void main(String args[]){
+        int numbers[]={1, -2, 6, -1, 3};
+        printSum(numbers);
+    }
+    
+}
