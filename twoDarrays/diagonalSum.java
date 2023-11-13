05b86438c87cf52091d6d5a3d6ce003c90f5287a
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-13 21:58:19 +0530

    diagonal sum

---
+++ twoDarrays/diagonalSum.java
@@ -0,0 +1,37 @@
+package twoDarrays;
+
+public class diagonalSum {
+    public static int DSum(int matrix[][]){
+        int sum =0;
+        //time complexity = O(n^2) or brute force
+        // for(int i=0; i<matrix.length; i++){
+        //     for(int j=0; j<matrix[0].length; j++){
+        //         if(i==j){
+        //             sum += matrix[i][j];
+        //         }
+        //         else if(i+j == matrix.length-1){
+        //             sum += matrix[i][j];
+        //         }
+        //     }
+        //  }
+        
+        //time complexity = O(n)
+        for(int i=0; i<matrix.length; i++){
+            //primary diagonal
+            sum+= matrix[i][i];
+            // secondary diagonal
+            if(i != matrix.length-1-i)
+                sum+=matrix[i][matrix.length-i-1];
+        }
+        return sum;
+        }
+    
+     public static void main(String args[]){
+        int matrix[][]={{1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 16}};
+        System.out.println(DSum(matrix));
+    }
+}
+
