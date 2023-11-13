97f46b7eea49b954c465cc06f1b2573e2f1b2d2d
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-13 22:52:15 +0530

    file added

---
+++ twoDarrays/searchSortedmatrix.java
@@ -0,0 +1,30 @@
+package twoDarrays;
+
+public class searchSortedmatrix {
+    //Staircase approach
+    public static boolean search(int matrix[][], int key){
+        int row =0, col = matrix[0].length-1;
+        while(row < matrix.length && col >= 0){
+            if(matrix[row][col]==key){
+                System.out.println("found key at: ("+row+","+col+")");
+                return true;
+            }
+            else if(key < matrix[row][col]){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        System.out.println("key not found!");
+        return false;
+    }
+    public static void main(String args[]){
+        int matrix[][]={{10, 20, 30, 40},
+                        {15, 25, 35, 45},
+                        {27, 29, 37, 48},
+                        {32, 33, 39, 50}};
+        search(matrix, 33);
+    
+    }
+}
