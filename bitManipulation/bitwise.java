59348b73d874e346dbada42c21c55a83c156345a
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-14 21:58:32 +0530

    add

---
+++ bitManipulation/bitwise.java
@@ -0,0 +1,13 @@
+package bitManipulation;
+
+public class bitwise {
+    public static void main(String args[]){
+        System.out.println(5 & 6);
+        System.out.println(5 | 6);
+        System.out.println(5 ^ 6);
+        System.out.println(~5);
+        System.out.println(5<<2);
+        System.out.println(5>>2);
+    }
+    
+}
