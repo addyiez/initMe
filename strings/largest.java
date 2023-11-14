ea813c4bdff29564ebcab69b95cf2fdca23bb685
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-14 18:15:31 +0530

    add.

---
+++ strings/largest.java
@@ -0,0 +1,16 @@
+package strings;
+
+public class largest {
+    public static void main(String args[]){
+        String fruits[] ={"apple", "mango", "banana"};
+
+        String largest = fruits[0];
+        for(int i=1; i<fruits.length; i++){
+            if(largest.compareTo(fruits[i])<0){
+                largest = fruits[i];
+            }
+        }
+        System.out.println(largest);
+    }
+    
+}
