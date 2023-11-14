37be870ed2bdd7294ad8811006d64d5bdd2935c0
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-14 17:37:06 +0530

    add

---
+++ strings/shortestPath.java
@@ -0,0 +1,35 @@
+package strings;
+
+public class shortestPath {
+    public static float getShortestPath(String path){
+        int x= 0, y= 0;
+        for(int i=0; i<path.length(); i++){
+            char dir = path.charAt(i);
+
+            //south
+            if(dir =='S'){
+                y--;
+            }
+            //north
+            else if(dir == 'N'){
+                y++;
+            }
+            //west
+            else if(dir == 'W'){
+                x--;
+            }
+            //east
+            else {
+                x++;
+            }
+        }
+        int X2 = x*x;
+        int Y2 = y*y;
+        return (float)Math.sqrt(X2 + Y2);
+    }
+    public static void main(String[] args) {
+        String path = "WNEENESENNN";
+        System.out.println(getShortestPath(path));
+    }
+    
+}
