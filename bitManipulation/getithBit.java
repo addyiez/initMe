261c1017e6ab906a43f4e810e314ffd6cbcac0ae
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-14 22:22:17 +0530

    .

---
+++ bitManipulation/getithBit.java
@@ -0,0 +1,16 @@
+package bitManipulation;
+
+public class getithBit {
+    public static int getIthBit(int n, int i){
+        int bitmask = 1<<i;
+        if((n & bitmask) == 0){
+            return 0;
+        }
+        else
+        return 1;
+    }
+    public static void main(String args[]){
+        System.out.println(getIthBit(10, 2));
+    }
+    
+}
