c6ad102486c68f9f423abd3fba5739695ec03714
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-17 05:09:39 +0530

    new

--- OOprograming/constructr.java
+++ OOprograming/constructr.java
@@ -15,3 +15,20 @@ class Student{
         this.name = name;
     }
 }
+/*
+ * 
+    public static void main(String args[]){
+        Student s1 = new Student();
+        //System.out.println(s1.name);
+    }
+    
+}
+class Student{
+    String name;
+    int roll;
+
+    Student(){
+        System.out.println("constructor is called....");
+    }
+}
+ */
