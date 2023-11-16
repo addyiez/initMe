6ddafac7525fcc24d7479670e4b47bcf0be1e5f4
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-17 03:51:33 +0530

    add

---
+++ OOPs/accessModifier.java
@@ -0,0 +1,21 @@
+package OOPs;
+
+public class accessModifier {
+
+  
+
+    public static void main(String args[]){
+        Bankaccount myAcc = new Bankaccount();
+        myAcc.username = "adityadeopaul";
+        myAcc.setPassword("abcdefghi");
+
+    }
+    
+}  
+class Bankaccount{
+        public String username;
+        private String password;
+        public void setPassword(String pwd){
+            password = pwd;
+        }
+    }
