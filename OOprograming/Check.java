413e055c54ca74eb8f3a99fdf652627237186a72
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-17 23:57:04 +0530

    add

---
+++ OOprograming/Check.java
@@ -0,0 +1,16 @@
+package OOprograming;
+
+public class Check {
+    public static void main(String args[]){
+        Test t = new Test();
+        t.set_marks(98);
+        System.out.println(Test.marks);
+    }    
+}
+class Test{
+    static int marks;
+    void set_marks(int marks){
+        //this.marks = marks;
+        Test.marks = marks;
+    }
+}
