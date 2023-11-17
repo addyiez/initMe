6e1e637d5fa3353efd8a5eafc079f770b57dfe73
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-18 00:48:53 +0530

    add

---
+++ recursion/factorial.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class factorial {
+    public static int fact(int n){
+        if(n == 0){
+            return 1;
+        }
+        //int f(n-1) = fact(n-1);
+        int fn = n * fact(n-1);
+        return fn;
+    }
+    public static void main(String args[]){
+        int n = 5;
+        System.out.println(fact(n));
+    }
+    
+}
