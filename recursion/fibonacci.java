a0ccfdd625ea5c42620bf28fd25b72ea0b3b9dcc
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-18 22:35:32 +0530

    fibonacci

---
+++ recursion/fibonacci.java
@@ -0,0 +1,19 @@
+package recursion;
+
+public class fibonacci {
+    public static int fib(int n){
+        if(n == 0 || n == 1){
+            return n;
+        }
+        // int fnm1 = fib(n-1);
+        // int fnm2 = fib(n-2);
+        // int fn = fnm1 + fnm2;
+        // return fn;
+        return fib(n-1) + fib(n-2);
+    }
+    public static void main(String args[]){
+       // int n = 5;
+        System.out.println(fib(5));
+    }
+    
+}
