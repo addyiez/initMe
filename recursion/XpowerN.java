7e5aa84bb07c7f0e5e31dea9ab9573084f73b32e
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-19 02:17:35 +0530

    Optimized

--- recursion/XpowerN.java
+++ recursion/XpowerN.java
@@ -1,18 +1,35 @@
 package recursion;
 
 public class XpowerN {
-    public static int power(int x, int n){
+    public static int power(int x, int n){ // O(n)
         if(n == 0){
             return 1;
         }
         // int xnm1 = power(x, n-1);
         // int xn = x * xnm1;
         // return xn;
-        
+
         return x * power(x, n-1);
+    }
+    public static int OptPower(int a, int n){ //optimozed code
+        //O(logn)- time complexity
+        if(n == 0){
+            return 1;
+        }
+        //int halfPowersq = OptPower(a, n/2) * OptPower(a, n/2);
+        int halfPowr = OptPower(a, n/2);
+        int halfPowersq = halfPowr * halfPowr;
+
+        // n is Odd
+        if(n % 2 != 0){
+            halfPowersq = a * halfPowersq;
+        }
+        return halfPowersq;
     }
     public static void main(String args[]){
         System.out.println(power(2, 3));
+        System.out.println(OptPower(2, 5));
+        
     }
     
 }
