571625cd8871d3d445510ea692046d9a5b40940d
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-19 06:14:28 +0530

    add

---
+++ DivideNconquror/QuickSort.java
@@ -0,0 +1,45 @@
+package DivideNconquror;
+
+public class QuickSort {
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void sort(int arr[], int si, int ei){
+        if(si >= ei){
+            return;
+        }
+        //last element
+        int pIdx = partition(arr, si, ei);
+        sort(arr, si, pIdx-1); //left
+        sort(arr, pIdx+1, ei); //right
+    }
+    public static int partition(int arr[], int si, int ei){
+        int pivot = arr[ei];
+        int i = si-1; //to make place for els smaller than pivot
+
+        for(int j=si; j<ei; j++){
+            if(arr[j] <= pivot){
+                i++;
+                //swap
+                int temp = arr[j];
+                arr[j] =arr[i];
+                arr[i] =temp;
+            }
+        }
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];  //pivot = arr[i] -> dont do this
+        arr[i] =temp;
+        return i;
+    }
+    public static void main(String[] args) {
+        int arr[] = {6, 3, 9, 8, 2, 5};
+        sort(arr, 0, arr.length-1);
+        printArr(arr);
+    }
+    
+}
