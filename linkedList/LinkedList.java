8deece3ca5dabe12e8115c6754ac8af774d08a6e
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-25 01:34:54 +0530

    search key via recursion

--- linkedList/LinkedList.java
+++ linkedList/LinkedList.java
@@ -126,6 +126,22 @@ public class LinkedList{
         return -1;
     }
 
+    public int helper(Node head, int key){ //O(n)
+        if(head == null){
+            return -1;
+        }
+        if(head.data == key){
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if(idx == -1){
+            return -1;
+        }
+        return idx+1;
+    }
+    public int recSearch(int key){
+        return helper(head, key);
+    }
     public static void main(String args[]){
         LinkedList ll = new LinkedList();
         ll.print();
@@ -145,15 +161,18 @@ public class LinkedList{
         //The static field LinkedList.size should be accessed in a static way
         //System.out.println(LinkedList.size);
 
-        ll.removeFirst();
-        ll.print();
+        // ll.removeFirst();
+        // ll.print();
 
-        ll.removeLast();
-        ll.print();
-        System.out.println(LinkedList.size);
+        // ll.removeLast();
+        // ll.print();
+        // System.out.println(LinkedList.size);
 
-        System.out.println(itrSearch(3));
-        System.out.println(itrSearch(10));
+        // System.out.println(itrSearch(3));
+        // System.out.println(itrSearch(10));
+
+        System.out.println(ll.recSearch(9));
+        System.out.println(ll.recSearch(10));
     }
 }
    
