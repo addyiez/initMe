6e781c40d5eaf53e9d4ed0791b0c922a8862f9c9
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-25 00:32:42 +0530

    introduction

---
+++ linkedList/LinkedList.java
@@ -0,0 +1,14 @@
+package linkedList;
+public class LinkedList{
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
+   
+
