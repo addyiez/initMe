7338e8e4b4186c1cfca36ba2bcba5cd7b6aa4032
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-26 00:46:27 +0530

    detect cycle in linked list

---
+++ linkedList/FloydsCFA.java
@@ -0,0 +1,38 @@
+package linkedList;
+
+public class FloydsCFA {
+
+    public static Node head;
+
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static boolean isCycle(){ //floyd's cycle finding algorithm
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next; //+1
+            fast = fast.next.next; //+2
+            if(slow == fast){
+                return true; //cycle exist
+            }
+        }
+        return false; //cycle doesnot exist
+    }
+    public static void main(String args[]){
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = head;
+        //1->2->3->1
+        System.out.println(isCycle());
+    }
+    
+}
