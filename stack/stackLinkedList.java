6dd4abe9b4613c83eb4eb0546bb4fd4c07a22b8f
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-26 23:37:22 +0530

    using java collection framework

--- stack/stackLinkedList.java
+++ stack/stackLinkedList.java
@@ -1,57 +1,72 @@
 package stack;
-
+import java.util.*;
 
 public class stackLinkedList {
-    static class Node{
-        int data;
-        Node next;
-        Node(int data){
-            this.data = data;
-            this.next = null;
-        }
-    }
-    static class Stack {
-        static Node head = null;
-        public static boolean isEmpty(){
-            return head == null;
-        }
-        //push
-        public static void push(int data){
-            Node newNode = new Node(data);
+    // static class Node{
+    //     int data;
+    //     Node next;
+    //     Node(int data){
+    //         this.data = data;
+    //         this.next = null;
+    //     }
+    // }
+    // static class Stack {
+    //     static Node head = null;
+    //     public static boolean isEmpty(){
+    //         return head == null;
+    //     }
+    //     //push
+    //     public static void push(int data){
+    //         Node newNode = new Node(data);
 
-            if(isEmpty()){
-                head = newNode;
-                return;
-            }
-            newNode.next = head;
-            head = newNode;
-        }
-        //pop
-        public static int pop(){
-            if(isEmpty()){
-                return -1;
-            }
-            int top = head.data;
-            head = head.next;
-            return top;
-        }
-        //peek
-        public static int peek(){
-            if(isEmpty()){
-                return -1;
-            }
-            return head.data;
-        }
-    }
+    //         if(isEmpty()){
+    //             head = newNode;
+    //             return;
+    //         }
+    //         newNode.next = head;
+    //         head = newNode;
+    //     }
+    //     //pop
+    //     public static int pop(){
+    //         if(isEmpty()){
+    //             return -1;
+    //         }
+    //         int top = head.data;
+    //         head = head.next;
+    //         return top;
+    //     }
+    //     //peek
+    //     public static int peek(){
+    //         if(isEmpty()){
+    //             return -1;
+    //         }
+    //         return head.data;
+    //     }
+    // }
+    // public static void main(String args[]){
+    //     Stack.push(1);
+    //     Stack.push(2);
+    //     Stack.push(3);
+
+    //     while(! Stack.isEmpty()){
+    //         System.out.println(Stack.peek());
+    //         Stack.pop();
+    //     }
+    // }
+
+    //Using java collection framework
     public static void main(String args[]){
-        Stack.push(1);
-        Stack.push(2);
-        Stack.push(3);
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
 
-        while(! Stack.isEmpty()){
-            System.out.println(Stack.peek());
-            Stack.pop();
+        while(! s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
         }
     }
+
+
     
 }
