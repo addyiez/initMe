f38a25a4b9e6611df9c3d77de15cc6bba7f4b868
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-26 22:25:02 +0530

    doubly LinkedList

---
+++ linkedList/doubleLL.java
@@ -0,0 +1,72 @@
+package linkedList;
+
+public class doubleLL {
+    public class Node{
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    //add
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+    //print
+    public void print(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data +"<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    //remove
+    public int removeFirst(){
+        if(head == null){
+            System.out.println("DLL is empty");
+            return Integer.MIN_VALUE;
+        }
+        if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
+    public static void main(String args[]){
+        doubleLL dll =  new doubleLL();
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+
+        dll.print();
+        System.out.println(doubleLL.size);
+
+        dll.removeFirst();
+        dll.print();
+        System.out.println(doubleLL.size);
+    }
+    
+}
