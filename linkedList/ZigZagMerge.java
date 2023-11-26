6eda61c87d5c33bc611d31413c4dbd1a03db6184
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-26 21:28:46 +0530

    add

---
+++ linkedList/ZigZagMerge.java
@@ -0,0 +1,81 @@
+package linkedList;
+
+public class ZigZagMerge {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+
+    public static void printLinkedList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void zigZag() {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        // find mid
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+
+        // reverse 2nd half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+
+        // alt merge - zigZag merge
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            left = nextL;
+            right = nextR;
+        }
+    }
+
+    public static void main(String args[]) {
+
+        head = new Node(5);
+        head.next = new Node(4);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(2);
+        head.next.next.next.next = new Node(1);
+
+        System.out.println("Original Linked List:");
+        printLinkedList(head);
+
+        zigZag();
+
+        System.out.println("ZigZag Merged Linked List:");
+        printLinkedList(head);
+    }
+}
