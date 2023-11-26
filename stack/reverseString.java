d2910db66ae6cf49e2101f1b44f78ef32383a2ef
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-27 00:19:36 +0530

    reverse a string

---
+++ stack/reverseString.java
@@ -0,0 +1,25 @@
+package stack;
+import java.util.*;
+
+public class reverseString {
+    public static String reverse(String str){
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while(idx < str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder result = new StringBuilder("");
+        while(! s.isEmpty()){
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return str = result.toString();
+    }
+    public static void main(String args[]){
+        String str = "ABCD";
+        String result = reverse(str);
+        System.out.println(result);
+    }
+    
+}
