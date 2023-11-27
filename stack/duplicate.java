3d5636276de9c7ec77cae6fc0a9461504a95a51e
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-27 19:48:44 +0530

    duplicate parentheses

---
+++ stack/duplicate.java
@@ -0,0 +1,35 @@
+package stack;
+import java.util.*;
+public class duplicate { //O(n)
+    public static boolean isDuplicate(String str){
+        Stack<Character> s = new Stack<>();
+
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            //closing
+            if(ch == ')'){
+                int count = 0;
+                while(s.peek() != '('){
+                    s.pop();
+                    count++;
+                }
+                if(count<1){
+                    return true; //duplicate
+                }else{
+                    s.pop(); //opening pair
+                }
+            }else{
+                //opning
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        //valid String
+        String str = "((a+b))"; //true
+        String str2 = "(a-b)"; //false
+        System.out.println(isDuplicate(str));
+        System.out.println(isDuplicate(str2));
+    }
+}
