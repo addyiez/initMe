6a88c5b31956d99c2e7dbde0698aaf6b9e8af3de
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-28 16:14:53 +0530

    implementation using ArrayDeque class

--- Queue/JCF.java
+++ Queue/JCF.java
@@ -3,7 +3,9 @@ import java.util.*;
 
 public class JCF {
      public static void main(String args[]){
-        Queue<Integer> q = new LinkedList<>(); // or implement with ArrayDeque class
+        //Queue<Integer> q = new LinkedList<>(); 
+        // or implement with ArrayDeque class
+        Queue<Integer> q = new ArrayDeque<>();
         q.add(1);
         q.add(2);
         q.add(3);
