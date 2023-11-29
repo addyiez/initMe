77abc096f7baa6f142c38e7f55c1365418c91772
addyiez <88774507+addyiez@users.noreply.github.com>
2023-11-30 04:54:11 +0530

    activity selection(end time sorted)

---
+++ Greedy/ActivitySelection.java
@@ -0,0 +1,31 @@
+package Greedy;
+import java.util.*;
+
+public class ActivitySelection { //O(n)
+    public static void main(String args[]){
+        int start[] = {1, 3, 0, 5, 8, 5};
+        int end[] = {2, 4, 6, 7, 9, 9};
+
+        //end time basis sorted
+        int maxAct = 0;
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        //1st activity 
+        maxAct = 1;
+        ans.add(0);
+        int lastEnd = end[0];
+        for(int i=1; i<end.length; i++){
+            if(start[i] >= lastEnd){
+                //activity select
+                maxAct++;
+                ans.add(i);
+                lastEnd = end[i];
+            }
+        }
+        System.out.println("max activities = "+maxAct);
+        for(int i=0; i<ans.size(); i++){
+            System.out.print("A"+ans.get(i)+" ");
+        }
+        System.out.println();
+    }
+}
